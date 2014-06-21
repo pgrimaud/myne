c064d3a487217beb74dc280ed6e3646875e825a6
Pierre Grimaud <pierre@shopping-flux.com>
2014-06-21 11:39:13 +0200

    [API] Core

--- api/model/class.api.php
+++ api/model/class.api.php
@@ -4,14 +4,13 @@ Class Api{
   
   private $get = array();
   private $post = array();
+  private $params;
   
-  public function __constuct() {
-    
-    show($_SERVER);
+  public function __construct() {
     
     $this->get = $_GET;
 		$this->post = $_POST;
-		
+
 		$request_method = $_SERVER['REQUEST_METHOD'];
 		$this->params = explode('/', $_SERVER['REQUEST_URI']);
 		
@@ -33,6 +32,20 @@ Class Api{
   }
   
   public function auth(){
+    
+  }
+  
+  public function error($message){
+    
+    header("HTTP/1.0 404 Not Found");
+		header("Content-type: text/xml; charset=utf-8");
+    
+		$xml = new SimpleXMLElement("<?xml version='1.0' ?>"."\n"."<error></error>");
+		$xml->addChild('message', $message);
+		
+		echo $xml->asXML();
+		
+		exit;		
     
   }
   
